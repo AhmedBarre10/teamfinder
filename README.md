05f5be03ebf400e940aeea6efc7cb659ca85a383
Carlos Alba <soradev4@gmail.com>
2021-04-23 20:39:34 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# teamfinder
-# teamfinder
+# Team Finder Mono Repo
---
+++ packages/web/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <h1>This is a home page</h1>
+    </div>
+  )
+}
